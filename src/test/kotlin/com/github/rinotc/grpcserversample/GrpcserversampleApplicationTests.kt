d1c3aa8e29b1e6d44ee871482281f0d32b2e4951
toshinori.chiba <toshinori.chiba.p@gmail.com>
2025-03-09 14:21:48 +0900

    initial commit

---
+++ src/test/kotlin/com/github/rinotc/grpcserversample/GrpcserversampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.rinotc.grpcserversample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GrpcserversampleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
